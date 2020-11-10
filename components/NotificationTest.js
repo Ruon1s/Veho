5d0ea459536409f9491e938e3a9f6a5e67147702
Matias Jalava <matias@elm.fi>
2020-11-10 10:22:10 +0200

    added expo notifications, no funtionality yet

---
+++ components/NotificationTest.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Button, Input, Form, Text, Item, Label, Content } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import GlobalStyles from '../styles/GlobalStyles';
+import * as Notifications from 'expo-notifications';
+
+const createNotification = () => {
+
+}
+
+const NotificationTest = () => {
+    React.useEffect(() => {
+        const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+            const url = response.notification.request.content.data.url;
+            Linking.openUrl(url);
+        });
+        return () => subscription.remove();
+    }, []);
+
+    return (
+        <Button
+            style={GlobalStyles.button}
+            transparent
+            full
+            onPress={createNotification}>
+            <Text>send notification</Text>
+        </Button>
+    )
+}
+
+export default NotificationTest
