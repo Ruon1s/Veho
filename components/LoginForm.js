316d4a6314bf971e29dee0c971bb26504c17a811
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2020-11-16 16:25:43 +0200

    ducktapeversion

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,22 +2,57 @@ import React from 'react';
 import { Container, Button, Input, Form, Text, Item, Label, Content } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import GlobalStyles from '../styles/GlobalStyles';
+import useLoginForm from '../hooks/LoginHook';
+import firebase from 'firebase'
+
 
 const LoginForm = ({ navigation, toResetPassword, toRegister }) => {
+    const {
+        handlePasswordChange,
+        handleEmailChange,
+        inputs
+    } = useLoginForm();
+
+    const login = async () => {
+        let check = true;
+       await firebase.auth().signInWithEmailAndPassword(inputs.email, inputs.password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+            check = false;
+        });
+        if(check === true){
+            navigation.replace('App')
+        }else {
+            console.log("sumtingwong")
+        }
+    };
+
+
     return (
         <Form>
             <Item floatingLabel>
                 <Label>Email</Label>
-                <Input keyboardType="email-address" />
+                <Input
+                keyboardType="email-address"
+                value={inputs.email}
+                onChangeText={handleEmailChange}
+                />
             </Item>
             <Item floatingLabel>
                 <Label>Password</Label>
-                <Input secureTextEntry={true} />
+                <Input
+                secureTextEntry={true}
+                value={inputs.password}
+                onChangeText={handlePasswordChange}
+                />
             </Item>
 
             <Grid>
                 <Col>
-                    <Button style={GlobalStyles.button} full onPress={() => navigation.replace('App')}>
+                    <Button style={GlobalStyles.button} full onPress={login}>
                         <Text>Login</Text>
                     </Button>
                 </Col>
