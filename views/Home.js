5d8e511b58ed5a5700dd0d5b87232de2abfbaa2a
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2020-11-18 17:15:17 +0200

    Cleaned the Home.js, small changes to logic etc..

--- views/Home.js
+++ views/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Text, Button, View, StyleProvider, Toast, Root } from 'native-base';
+import { Container, Text, Button, View, StyleProvider, Toast, Root, Spinner } from 'native-base';
 import { Col, Grid } from 'react-native-easy-grid';
 import getTheme from '../native-base-theme/components';
 import platform from '../native-base-theme/variables/platform';
@@ -9,66 +9,29 @@ import QueueInfo from '../components/QueueInfo';
 import CustomHeader from '../components/CustomHeader';
 import { AUTH, GRANT, UNAME, PASS } from "@env";
 import * as SecureStore from 'expo-secure-store';
-import * as Notifications from 'expo-notifications';
 import { schedulePushNotification } from '../services/NotificationService';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import useQueueHooks from '../hooks/QueueHooks';
 
 const Home = ({ navigation }) => {
-    // State: for queue information
-    const [state, setState] = useState({
-        adding: false,                     //Disable the queue button while adding the document to firebase to prevent double documents
-        inQueue: false,                    //Change queue button depending this state
-        queue: 0,                          //How many users are in queue
-        freeSpots: [],                     //Store the document ID:s of the free spots (This arrays length also tells how many free spots there are)
-        queuePosition: 0,                  //The users own position in queue
-        charging: false,                   //If the user has the car already charging this will be true (Is the secure storage better?)
-        spotAvailable: false,              //If there is spot available
-    });
+    const [available, setAvailable] = useState();           //To check if there is a spot available right away
     const [batteryStatus, setBatteryStatus] = useState(54)
 
+    const { 
+        queue, 
+        parkingSpots, 
+        queueListener, 
+        parkingSpotListener, 
+        addUserToQueue,
+        removeUserFromQueue,
+        startCharging,
+        checkStatus,
+    } = useQueueHooks();
+
     useEffect(() => {
-        const unsubscribeQueueListener = firebase.firestore().collection('queue').orderBy('time', 'asc').onSnapshot(snapShot => {   //Check how many people are in the queue and update the state according to that. Also check if the user is found on the queue
-            
-            setState((state) => ({                                                                                                  //Get the queue size (How many documents there are in the collection)                                                                       
-                ...state,
-                queue: snapShot.size
-            }));
-
-            let placement = 0;                                                                                                      //Just to tell your spot in the queue (if it is even working). Need to come up with better later maybe
-
-            snapShot.forEach(document => {                                                                                          //Loop through the documents in the collection
-                const user = firebase.auth().currentUser;
-                const data = document.data();
-
-                placement++;
-
-                if (data.user_id === user.uid) {                                                                                    //If the user id is found in one of the documents check the position and change the state as needed
-                    setState((state) => ({
-                        ...state,
-                        inQueue: true,
-                        queuePosition: placement,
-                    }));
-                }
-            });
-        });
-
-        const unsubscribeParkingSpotListener = firebase.firestore().collection('parkingspots').where("availability", "==", true).onSnapshot(snapShot => {
-
-            setState(state => ({
-                ...state,
-                freeSpots: [],
-            }));
-
-            snapShot.forEach(document => {
-                setState(state => ({
-                    ...state,
-                    freeSpots: state.freeSpots.concat(document.id)
-                }));
-            }); 
-        });
-
-        getChargingDocId();
+        const unsubscribeQueueListener = queueListener();
+        const unsubscribeParkingSpotListener = parkingSpotListener();
 
         return () => {
             unsubscribeQueueListener();
@@ -76,41 +39,9 @@ const Home = ({ navigation }) => {
         }
     }, []);
 
-    useEffect(() => {                                                                                                       //Pretty awful looking if statement for checking if there are free spots available
-        if (!state.charging && state.freeSpots.length > 0 && (state.queue === 0 || state.queuePosition === 1)) {            //First check if the user is not charging and there are more spots available than 
-            Toast.show({                                                                                                    //Show a toast to the user
-                text: 'Free spot available right away!',
-                position: 'bottom',
-                duration: 3000,
-                type: 'success'
-            });
-
-            setState(state => ({                                                                                            //Change the needed states
-                ...state,
-                spotAvailable: true,
-            }));
-        } else {                    
-            setState(state => ({                                                                                            //Change the availability to false if the conditions change
-                ...state,
-                spotAvailable: false
-            }));
-        }
-    }, [state.freeSpots, state.queuePosition, state.queue]);
-
-    const getChargingDocId = async () => {                                                                                  //Function that gets the charging stations document id from secure store. If the document ID exists, it means that the user is charging their vehicle
-        try {
-            const docID = await SecureStore.getItemAsync('chargingDocId');
-
-            if (docID !== null) {
-                setState(state => ({
-                    ...state,
-                    charging: true,
-                }));
-            }
-        } catch (error) {
-            console.log(`Error while getting chargingDoc: ${ error.message }`);
-        }
-    }
+    useEffect(() => {
+        setAvailable(checkStatus());
+    }, [parkingSpots, queue]);
 
     const fetchSoc = async () => {
         const token = await SecureStore.getItemAsync('token');
@@ -172,96 +103,6 @@ const Home = ({ navigation }) => {
         }
     }
 
-    const addToQueue = async () => {                                                        //Function that handles adding user to the queue
-        try {
-
-            setState((state) => ({                                                          //Change the state to adding to disable the button
-                ...state,
-                adding: true
-            }));
-
-            const user = firebase.auth().currentUser;                                       //Get the curren user
-            const timestamp = Date.now();                                                   //Create a timestamp
-            const token = (await Notifications.getExpoPushTokenAsync()).data;               //Get the token for the notifications
-            
-            const response = await firebase.firestore().collection('queue').add({           //Add a new document to the queue collection
-                time: timestamp,
-                user_id: user.uid,
-                pushNotificationToken: token,
-            });
-
-            await SecureStore.setItemAsync('queueId', response.id);                         //Store the documentID to secure store so we can delete is later if the users leaves from the queue or the user starts charging
-
-            setState((state) => ({                                                          //Handle rest of state changes (inQueue true to change the button, adding false, since the user has been added to the queue)
-                ...state,
-                inQueue: true,
-                adding: false,
-            }));
-
-            Toast.show({                                                                    //Notify user about successful action with the toast!
-                text: 'You are added to the queue!',
-                position: 'bottom',
-                duration: 3000,
-                type: 'success',
-            });
-        } catch (error) {
-            console.log(`Error adding user to the queue: ${ error.message }`);
-        }
-    }
-
-    const removeFromQueue = async () => {                                                   //Function that will remove the user from queue
-        try {
-            const docId = await SecureStore.getItemAsync('queueId');                        //Get the docId to delete the right one
-            await firebase.firestore().collection('queue').doc(docId).delete();             //Delete the document
-
-            setState((state) => ({                                                          //Change needed states back to default to handle the buttons etc..
-                ...state,
-                inQueue: false,
-                queuePosition: 0,
-            }));
-
-            Toast.show({                                                                    //Notify user about successful action with the toast!
-                text: 'You were removed from the queue',
-                position: 'bottom',
-                duration: 3000,
-                type: 'success'
-            });
-        } catch (error) {
-            console.log(`Error while removing from queue: ${ error.message }`);
-        }
-    }
-
-    const startCharging = async () => {                                                     //Function that will take user to the charging view when there are free spots avalaible.
-        try {
-
-            setState(state => ({                                                            //Just to disable the button
-                ...state,
-                adding: true,
-            }));
-
-            const chargingSpotDocId = state.freeSpots[0];                                               //Get the first available charging spot
-            await SecureStore.setItemAsync('chargingDocId', chargingSpotDocId);                         //Save the chargingDocId to secure store so we know if the user is charging
-
-            if (state.inQueue) {
-                removeFromQueue();                                                                      //Delete user from the queue if the user is in the queue
-            }
-
-            await firebase.firestore().collection('parkingspots').doc(chargingSpotDocId).set({          //Switch the parkingspots availability to false
-                availability: false,
-            }, { merge: true });
-
-            setState(state => ({
-                ...state,
-                adding: false,
-                charging: true,
-            }));
-
-            navigation.navigate('ChargingView');                                            //Navigate to charging view
-        } catch (error) {
-            console.log(`Error while going to charging view: ${ error.message }`);
-        }
-    }
-
     const logout = async () => {                                                            //Functions that logs the user out (Need to be changed to Settings page later?)
         await firebase.auth().signOut();
         navigation.replace('Auth');
@@ -274,7 +115,7 @@ const Home = ({ navigation }) => {
                     <CustomHeader title='Home' />
 
                     <View padder style={{ flex: 1, justifyContent: 'space-between', marginBottom: 60 }}>
-                        <QueueInfo free={state.freeSpots.length} queue={state.queue} queuePosition={state.queuePosition} />
+                        <QueueInfo free={ parkingSpots.available.length } queue={ queue.size } queuePosition={ queue.position } />
                         <BatteryInfo batteryStatus={batteryStatus} />
 
                         <Button full transparent onPress={() => schedulePushNotification('Test', 'Hello', 123)}>
@@ -290,36 +131,36 @@ const Home = ({ navigation }) => {
                                 style={GlobalStyles.button}>
                                 <Text>(DEV) Refresh SOC</Text>
                             </Button>
-                            {state.inQueue && state.spotAvailable ?
+                            {queue.inQueue && available ?
                             <>
-                                <Button full style={ GlobalStyles.button } onPress={ startCharging } >
+                                <Button full style={ GlobalStyles.button } onPress={ () => startCharging(navigation) } >
                                     <Text>Start Charging</Text>
                                 </Button>
-                                <Button full style={ GlobalStyles.button } onPress={ removeFromQueue }>
-                                    <Text>Leave Queue</Text>
+                                <Button full style={ GlobalStyles.button } onPress={ removeUserFromQueue }  disabled={ queue.processing }>
+                                    { queue.processing ? <Spinner /> : <Text>Leave Queue</Text> }
                                 </Button>
                             </>
                             :
                             null}
-                            {!state.inQueue && !state.spotAvailable && !state.charging ? 
-                            <Button full style={ GlobalStyles.button } onPress={ addToQueue } disabled={ state.adding }>
-                                <Text>Queue</Text>
+                            {!queue.inQueue && !available && !parkingSpots.inSpot ? 
+                            <Button full style={ GlobalStyles.button } onPress={ addUserToQueue } disabled={ queue.processing }>
+                                { queue.processing ? <Spinner /> : <Text>Queue</Text> } 
                             </Button>
                             :
                             null}
-                            {state.inQueue && !state.spotAvailable ?
-                            <Button full style={ GlobalStyles.button } onPress={ removeFromQueue }>
-                                <Text>Leave Queue</Text>
+                            {queue.inQueue && !available ?
+                            <Button full style={ GlobalStyles.button } onPress={ removeUserFromQueue } disabled={ queue.processing }>
+                                { queue.processing ? <Spinner /> : <Text>Leave Queue</Text> }
                             </Button>
                             :
                             null}
-                            {!state.inQueue && state.spotAvailable ?
-                            <Button full style={ GlobalStyles.button } onPress={ startCharging }>
+                            {!queue.inQueue && available ?
+                            <Button full style={ GlobalStyles.button } onPress={ () => startCharging(navigation) }>
                                 <Text>Start Charging</Text>
                             </Button>
                             :
                             null}
-                            {state.charging ?
+                            {parkingSpots.inSpot ?
                             <Button full style={ GlobalStyles.button } onPress={ () => navigation.navigate('ChargingView') }>
                                 <Text>To Charging View</Text>
                             </Button>
