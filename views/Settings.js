68902d37cec13d34eda09ee7654ed374ca188cf1
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2020-11-23 16:32:01 +0200

    no vin in settings

--- views/Settings.js
+++ views/Settings.js
@@ -11,18 +11,6 @@ import GlobalStyles from "../styles/GlobalStyles";
 
 
 const Settings = ({navigation}) => {
-    const {
-        getCarVin,
-        carVin,
-        setCarVin,
-        changeEditable,
-        editable,
-        editCarVin
-    } = useSettingsForm();
-
-    useEffect(() => {
-        getCarVin().then(r => setCarVin(r))
-    }, []);
 
     const toAddCar = () => {
         navigation.navigate('AddCarDetails')
@@ -41,41 +29,6 @@ const Settings = ({navigation}) => {
                         <Text>Add new car</Text>
                     </Button>
 
-
-                    <Form>
-                        <Item floatingLabel>
-                            <Label>Edit your car vin</Label>
-                            {editable === false ?
-                                <Input
-                                    autoCapitalize='none'
-                                    editable={false}
-                                    defaultValue={carVin}
-                                    value={carVin}
-                                /> :
-                                <Input
-                                    autoCapitalize='none'
-                                    editable={true}
-                                    defaultValue={carVin}
-                                />
-                            }
-                        </Item>
-                        {editable === false ?
-                            <Button
-                                full
-                                style={GlobalStyles.button}
-                                onPress={changeEditable}>
-                                <Text>edit</Text>
-                            </Button>
-                            :
-                            <Button
-                                full
-                                style={GlobalStyles.button}
-                                onPress={editCarVin}>  //do 1 function that runs 2 functions editCarVin and changeEditable
-                                <Text>Save</Text>
-                            </Button>
-                        }
-
-                    </Form>
                 </Content>
             </Container>
         </StyleProvider>
