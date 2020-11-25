1cab754ee463dbbb78824ec81980fdcf558d3bfe
Matias Jalava <matias@elm.fi>
2020-11-25 13:16:27 +0200

    layout changes to managerView

--- components/HomeListLayout.js
+++ components/HomeListLayout.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Content, Text, Button, View, StyleProvider, Card, CardItem, Body, List, ListItem } from 'native-base';
-import { Col, Grid, Row } from 'react-native-easy-grid';
-import BatteryInfo from './BatteryInfo';
+import { Content, Text, View, Card, CardItem } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const HomeListLayout = (props) => {
@@ -12,21 +10,16 @@ const HomeListLayout = (props) => {
         {carArray.map((car, key) => (
             <Card key={key}>
                 <CardItem button onPress={() => alert('Pressed ' + car.name)}>
-                    <Body>
-                        <View style={styles.parent}>
-
-                            <Text style={styles.carName}>{car.name.toUpperCase()}</Text>
-
-                            <View style={styles.licenceNumber}>
-                                <Text style={{ fontSize: 32, alignSelf: 'center' }}>{car.licencePlate.toUpperCase()}</Text>
-                            </View>
-
-                            <View style={styles.batteryStatus}>
-                                <Icon name='bolt' size={42} color="#000" style={{ marginStart: 12 }} />
-                                <Text style={{ margin: 8, fontSize: 24 }}>42%</Text>
-                            </View>
-                        </View>
-                    </Body>
+                    <View style={styles.left}>
+                        <Text numberOfLines={1} style={styles.carName}>{car.name.toUpperCase()}</Text>
+                    </View>
+                    <View style={styles.bodyStyle}>
+                        <Text numberOfLines={1} style={styles.licenceNumber}>{car.licencePlate.toUpperCase()}</Text>
+                    </View>
+                    <View style={styles.batteryStatus}>
+                        <Icon name='bolt' size={32} color="#EAEAEA" />
+                        <Text numberOfLines={1} style={styles.batteryPercentage}>42%</Text>
+                    </View>
                 </CardItem>
             </Card>
         ))}
@@ -34,25 +27,31 @@ const HomeListLayout = (props) => {
 }
 
 const styles = StyleSheet.create({
-    parent: {
-        flex: 1,
-        flexDirection: 'row'
+    left: {
+        flex: 1
     },
     carName: {
-        fontSize: 32,
-        marginEnd: 12
+        fontSize: 26
+    },
+    bodyStyle: {
+        flex: 1,
+        borderColor: '#EAEAEA',
+        borderStartWidth: 1,
+        borderEndWidth: 1
     },
     licenceNumber: {
-        flex: 44,
-        borderColor: '#dedede',
-        borderStartWidth: 1,
-        borderEndWidth: 1,
-        alignContent: 'center',
-        fontSize: 32
+        alignSelf: 'center',
+        fontSize: 26
     },
     batteryStatus: {
-        flex: 28,
+        flex: 0.7,
+        justifyContent: 'flex-end',
+        display: 'flex',
         flexDirection: 'row'
+    },
+    batteryPercentage: {
+        fontSize: 26,
+        marginLeft: 8
     }
 });
 
