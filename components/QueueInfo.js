43adfb8dcc6df5ad5e2c163711eed1febb8da841
Matias Jalava <matias@elm.fi>
2020-12-09 11:08:14 +0200

    cleaned up QueueInfo-component

--- components/QueueInfo.js
+++ components/QueueInfo.js
@@ -4,58 +4,6 @@ import { Text, View, Button } from 'native-base';
 import i18n from 'i18n-js';
 
 const QueueInfo = (props) => {
-    const charging = props.charging
-
-    const [state, setState] = useState({
-        height: null,
-        fontSize: 24,
-        borderBottomWidth: 1,
-        padding: 12
-    })
-
-    useEffect(() => {
-        /* switch (charging) {
-            case (true): {
-                // TODO: Invoke closing animation here
-                setState({
-                    height: 0,
-                    fontSize: 0,
-                    borderBottomWidth: 0,
-                    padding: 0
-                });
-                break;
-            }
-            default: {
-                // TODO: Invoke opening animation here
-                setState({
-                    height: null,
-                    fontSize: 24,
-                    borderBottomWidth: 1,
-                    padding: 12
-                });
-                break;
-            }
-        } */
-    }, [charging])
-
-    const styles = StyleSheet.create({
-        textParent: {
-            position: 'relative',
-            borderBottomColor: '#EAEAEA',
-            borderBottomWidth: state.borderBottomWidth,
-            paddingBottom: state.padding,
-            height: state.height,
-        },
-        infoText: {
-            fontSize: state.fontSize,
-        },
-        charginText: {
-            fontSize: state.fontSize,
-            color: '#4fd966',
-        }
-    });
-
-
     return (<View style={styles.textParent}>
         {props.queue > 0 ?
             <Text style={styles.infoText}>{i18n.t('numberOfCars')} {props.queue}</Text> : <Text style={styles.infoText}>{i18n.t('noCarsInQueue')}</Text>
@@ -68,9 +16,24 @@ const QueueInfo = (props) => {
         {props.charging ? <Text style={styles.infoText}>{i18n.t('currentlyCharging')}</Text> : props.queuePosition > 0 ?
             <Text style={styles.infoText}>{i18n.t('yourPosition')} {props.queuePosition}</Text> : <Text style={styles.infoText}>{i18n.t('notInQueue')}</Text>
         }
-
     </View>)
 }
+
+const styles = StyleSheet.create({
+    textParent: {
+        position: 'relative',
+        borderBottomColor: '#EAEAEA',
+        borderBottomWidth: 1,
+        paddingBottom: 12,
+    },
+    infoText: {
+        fontSize: 24,
+    },
+    charginText: {
+        fontSize: 24,
+        color: '#4fd966',
+    }
+});
 
 
 
