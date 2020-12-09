44baa87c93fdb3af0047aeb78b8f3eb006a72aad
Matias Jalava <matias@elm.fi>
2020-12-09 13:09:39 +0200

    small tweaks to carlist

--- components/CarList.js
+++ components/CarList.js
@@ -1,6 +1,7 @@
-import { Card, CardItem, Content, Text, View, Button } from "native-base";
+import { Card, CardItem, Content, Text, View, Button, Right } from "native-base";
 import React from "react";
 import useFirebase from "../hooks/FireBaseHook";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import i18n from 'i18n-js';
 
 const CarList = (props) => {
@@ -10,19 +11,21 @@ const CarList = (props) => {
     return (
         <Content padder>
             {carArray.map((car, key) => (
-                <Card key={key}>
+                <Card key={key} >
                     <CardItem style={{ display: "flex", flexDirection: 'row' }}>
                         <View style={{ flex: 1 }}>
                             <Text numberOfLines={1}>
                                 {car.name.toUpperCase()}
                             </Text>
                         </View>
-                        <View style={{ flex: 1 }}>
+                        <View style={{ flex: 1, alignItems: 'center' }}>
                             <Text numberOfLines={1}>{car.licencePlate.toUpperCase()}</Text>
                         </View>
                         <View style={{ flex: 1 }}>
-                            <Button danger transparent onPress={() => props.deleteCar(car)}>
-                                <Text>{i18n.t('delete')}</Text>
+                            <Button transparent onPress={() => props.deleteCar(car)}>
+                                <Right>
+                                    <Icon name="trash-alt" size={26} color='#FB3664'></Icon>
+                                </Right>
                             </Button>
                         </View>
                     </CardItem>
